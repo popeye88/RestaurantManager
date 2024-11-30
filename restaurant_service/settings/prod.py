0ba4e60b561d6c6891bc104bf31cc94d35b4a8cc
popeye88 <aleksei.taran@gmail.com>
2024-11-30 16:31:37 +0200

    added dump and deploy settings

--- restaurant_service/settings/prod.py
+++ restaurant_service/settings/prod.py
@@ -1,11 +1,13 @@
-import os
-
 from restaurant_service.settings.base import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
